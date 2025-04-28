5af2660b7bf4d5c889247efedb33c0ad5146d012
harshalkataria <harshalkataria14@gmail.com>
2025-04-28 13:49:27 +0400

    Fix the unwanted rotating box shadow issue

--- src/utils/version.js
+++ src/utils/version.js
@@ -1,13 +1,13 @@
 export const VERSION = {
     major: 2,
     minor: 0,
-    patch: 0,
+    patch: 1,
     toString() {
         return `${this.major}.${this.minor}.${this.patch}`;
     }
 };
 
-export const BUILD_DATE = new Date('2025-04-25');
+export const BUILD_DATE = new Date('2025-04-28');
 
 export const getVersionString = () => {
     return `v${VERSION.toString()} (${BUILD_DATE.toISOString().split('T')[0]})`;
